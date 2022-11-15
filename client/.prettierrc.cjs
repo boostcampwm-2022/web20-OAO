810a4e89f406ab116dcd05d746ff22e622955233
daeseong9388 <103937206+daeseong9388@users.noreply.github.com>
2022-11-15 22:06:18 +0900

    chore: prettier 옵션 추가

--- client/.prettierrc.cjs
+++ client/.prettierrc.cjs
@@ -1,6 +1,12 @@
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: 'all',
   tabWidth: 2,
   semi: true,
-  singleQuote: true
+  singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  arrowParens: 'always',
+  proseWrap: 'never',
 };
