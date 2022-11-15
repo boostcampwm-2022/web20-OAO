f19f15e5f4f213cc994367d759c644206e6b4ec2
kumsil1006 <39304306+kumsil1006@users.noreply.github.com>
2022-11-15 17:58:25 +0900

    feat: eslint 설정

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "airbnb-typescript", "airbnb/hooks", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+  },
+};
