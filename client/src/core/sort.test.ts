bdd5066af2d60b484f1e39321d80b62cb98b2c04
n-ryu <ababc100564@gmail.com>
2022-11-21 14:44:29 +0900

    test: importance 정렬과 deadline 정렬 검증 테스트 코드 작성 resolve #24 #27

--- client/src/core/sort.test.ts
+++ client/src/core/sort.test.ts
@@ -9,7 +9,7 @@ const generateRandomTodo = (): Todo => ({
   title: uuid(),
   content: '',
   owner: '',
-  priority: Math.ceil(Math.random() * 3),
+  importance: Math.ceil(Math.random() * 3),
   until: new Date(today.getTime() + Math.floor(Math.random() * WEEK)),
   from: new Date(1994, 10, 5),
   prev: [],
@@ -32,12 +32,42 @@ const validateImminence = (todoList: Array<Todo>): boolean => {
     .reduce((acc, el, i) => acc && el.index === i, true);
 };
 
-const validateImportance = (todoList: Array<Todo>): boolean => {
-  return false;
+const validateImportance = (todoList: Array<Todo>): boolean =>
+  todoList.reduce((acc, el, i, arr) => acc && (i === 0 || el.importance <= arr[i - 1].importance), true);
+
+const validateImminenceImportance = (todoList: Array<Todo>): boolean => {
+  if (!validateImminence(todoList)) return false;
+  const todayOnlyDate = onlyDate(today);
+  const imminentTodoList = todoList.filter((el) => todayOnlyDate === onlyDate(el.until));
+  const distantTodoList = todoList.filter((el) => todayOnlyDate !== onlyDate(el.until));
+  return validateImportance(imminentTodoList) && validateImportance(distantTodoList);
 };
 
 const validateDeadline = (todoList: Array<Todo>): boolean => {
-  return false;
+  return todoList.reduce((subAcc, todo, i, arr) => subAcc && (i === 0 || todo.until >= arr[i - 1].until), true);
+};
+
+const validateAll = (todoList: Array<Todo>): boolean => {
+  if (!validateImminenceImportance(todoList)) return false;
+  const properties = [
+    { imminence: true, importance: 3 },
+    { imminence: true, importance: 2 },
+    { imminence: true, importance: 1 },
+    { imminence: false, importance: 3 },
+    { imminence: false, importance: 2 },
+    { imminence: false, importance: 1 },
+  ];
+  const todayOnlyDate = onlyDate(today);
+  return properties.reduce(
+    (acc, el) =>
+      acc &&
+      validateDeadline(
+        todoList.filter(
+          (todo) => el.imminence === (todayOnlyDate === onlyDate(todo.until)) && el.importance === todo.importance,
+        ),
+      ),
+    true,
+  );
 };
 
 describe('기본 정렬 테스트', () => {
@@ -50,23 +80,14 @@ describe('기본 정렬 테스트', () => {
       expect(validateImminence(sort(todoList))).toBe(true);
     });
   });
-  describe('Importance 정렬', () => {
-    it('다른 조건이 모두 동일하다면, Importance가 높은 Todo가 Active된다.', () => {
-      expect(validateImportance(sort(todoList))).toBe(true);
-    });
-  });
-  describe('EDF 정렬', () => {
-    it('다른 조건이 모두 동일하고, Deadline이 빠른 Todo가 Active된다.', () => {
-      expect(validateDeadline(sort(todoList))).toBe(true);
-    });
-  });
   describe('Imminence + Importance 정렬', () => {
-    it('Imminence 정렬이 Importance 정렬보다 우선한다.', () => {});
-  });
-  describe('Importance + EDF 정렬', () => {
-    it('Importance 정렬이 EDF 정렬보다 우선한다.', () => {});
+    it('Imminence 정렬이 Importance 정렬보다 우선한다.', () => {
+      expect(validateImminenceImportance(sort(todoList))).toBe(true);
+    });
   });
   describe('Imminence + Importance + EDF', () => {
-    it('Imminence 정렬이 Importance 정렬보다 우선하며, Importance 정렬이 EDF 정렬보다 우선한다.', () => {});
+    it('Imminence 정렬이 Importance 정렬보다 우선하며, Importance 정렬이 EDF 정렬보다 우선한다.', () => {
+      expect(validateAll(sort(todoList))).toBe(true);
+    });
   });
 });
