1a0dbaa5f418bd2fb5882b611e9c231d0c8dcc68
daeseong9388 <ds.jeon@yonsei.ac.kr>
2022-11-22 15:15:07 +0900

    fix: Todo interface 수정

--- client/src/core/todo/index.ts
+++ client/src/core/todo/index.ts
@@ -7,14 +7,11 @@ export interface Todo {
   importance: number; // INT or ENUM, 할일의 우선순위 레벨
   until: Date; // DATE, 할일의 마감기한
   from: Date; // DATE, 할일의 시작기한
-  prev: Array<Todo>; // or Array<string>, 이전에 반드시 완료되어야 하는 할일 id 배열
-  next: Array<Todo>; // or Array<string>, 본 할일 이후에 실행되어야 하는 할일 id 배열
+  prev: Array<string>; // or Array<string>, 이전에 반드시 완료되어야 하는 할일 id 배열
+  next: Array<string>; // or Array<string>, 본 할일 이후에 실행되어야 하는 할일 id 배열
 
   // (필수) 할일의 상태값
-  isDone?: boolean; // 할일이 완료되었는지 여부
-  isReady?: boolean; // 할일이 시행할 준비가 되었는지 여부 (from, isAllPrevDone 체크)
-
-  isAllPrevDone?: boolean; // 선행해야하는 할일이 모두 완료되었는지 여부, prev를 Array<Todo>로 관리시 불필요
+  state: 'READY' | 'DONE' | 'WAIT';
 
   // (선택) 메타데이터
   spentTime?: Date; // 총 할일 수행 시간, default = 0
