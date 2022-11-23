a9b5c0b3531e697c055f7e8cb3eb564627e0f47a
n-ryu <ababc100564@gmail.com>
2022-11-23 12:10:24 +0900

    refactor: 정렬 테스트 이터레이션 구조로 변경

--- client/src/core/todo/test/sort.test.ts
+++ client/src/core/todo/test/sort.test.ts
@@ -5,24 +5,22 @@ const sort = (todoList: Array<TestTodo>): Array<TestTodo> => {
   return [...todoList];
 };
 
+const testCases = new Array(10).fill(0).map((el, i) => ({
+  tag: `sort/${i}`,
+  today: testToday,
+  data: generateTodoListForSortTest(Math.floor(Math.random() * 90) + 10),
+}));
+
 describe('기본 정렬 테스트', () => {
-  let todoList: Array<TestTodo>;
-  beforeEach(() => {
-    todoList = generateTodoListForSortTest(100);
-  });
-  describe('Imminence 정렬', () => {
+  describe.each(testCases)('$tag', ({ data, today }) => {
     it('다른 조건이 모두 동일하다면, Imminent Todo가 Active된다.', () => {
-      expect(validateImminenceSort(sort(todoList), testToday)).toBe(true);
+      expect(validateImminenceSort(sort(data), today)).toBe(true);
     });
-  });
-  describe('Imminence + Importance 정렬', () => {
     it('Imminence 정렬이 Importance 정렬보다 우선한다.', () => {
-      expect(validateImportanceSort(sort(todoList), testToday)).toBe(true);
+      expect(validateImportanceSort(sort(data), today)).toBe(true);
     });
-  });
-  describe('Imminence + Importance + EDF', () => {
     it('Imminence 정렬이 Importance 정렬보다 우선하며, Importance 정렬이 EDF 정렬보다 우선한다.', () => {
-      expect(validateDeadlineSort(sort(todoList), testToday)).toBe(true);
+      expect(validateDeadlineSort(sort(data), today)).toBe(true);
     });
   });
 });
