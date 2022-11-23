3f5903386b365538e5874afae5e8601c92c83479
daeseong9388 <ds.jeon@yonsei.ac.kr>
2022-11-23 14:40:39 +0900

    test: 업데이트 대단위 테스트 작성 resolve #49 #50

--- client/src/core/todo/test/sort.test.ts
+++ client/src/core/todo/test/sort.test.ts
@@ -38,7 +38,7 @@ const macroUnitTestCases = generateSortTestSet(sortTestCase, 5);
 
 describe('정렬 대단위 테스트', () => {
   describe.each(macroUnitTestCases)('$tag', ({ problem, today, answer }) => {
-    it('Imminence 정렬이 Importance 정렬보다 우선하며, Importance 정렬이 EDF 정렬보다 우선한다.', () => {
+    it('Ready Todo List의 기본적인 정렬을 할 수 있다.', () => {
       expect(sort(problem, today).map((el) => toComparableTestTodo(el))).toEqual(
         answer.map((el) => toComparableTestTodo(el)),
       );
--- client/src/core/todo/test/update.test.ts
+++ client/src/core/todo/test/update.test.ts
@@ -1,7 +1,15 @@
-import { TestTodo, generateTodoListForUpdateTest, testToday } from './type';
+import { TestTodo, generateTodoListForUpdateTest, testToday, toTestTodo, toComparableTestTodo } from './type';
 import { validateRTL, validateWTL } from './validator';
+import { generateSortTestSet } from './generator';
+import * as updateRawTestCase from './sort.data.json';
 
-const update = (todoList: Array<TestTodo>): Array<TestTodo> => {
+const updateTestCase = updateRawTestCase.map((el) => ({
+  tag: el.tag,
+  today: new Date(el.today),
+  data: el.data.map((todo) => toTestTodo(todo)),
+}));
+
+const update = (todoList: Array<TestTodo>, today: Date): Array<TestTodo> => {
   return [...todoList];
 };
 
@@ -14,10 +22,22 @@ const testCases = new Array(10).fill(0).map((el, i) => ({
 describe('업데이트 테스트', () => {
   describe.each(testCases)('$tag', ({ data, today }) => {
     it('RTL 조건 확인', () => {
-      expect(validateRTL(update(data), today)).toBe(true);
+      expect(validateRTL(update(data, today), today)).toBe(true);
     });
     it('WTL 조건 확인', () => {
-      expect(validateWTL(update(data), today)).toBe(true);
+      expect(validateWTL(update(data, today), today)).toBe(true);
+    });
+  });
+});
+
+const macroUnitTestCases = generateSortTestSet(updateTestCase, 5);
+
+describe('업데이트 대단위 테스트', () => {
+  describe.each(macroUnitTestCases)('$tag', ({ problem, today, answer }) => {
+    it('Todo List의 선후관계 및 날짜 비교를 통해 Todo들의 상태를 업데이트 할 수 있다.', () => {
+      expect(update(problem, today).map((el) => toComparableTestTodo(el))).toEqual(
+        answer.map((el) => toComparableTestTodo(el)),
+      );
     });
   });
 });
