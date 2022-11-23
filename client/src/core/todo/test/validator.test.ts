ad0a3618d7c270b096d17d56e997dffff995441e
n-ryu <ababc100564@gmail.com>
2022-11-23 12:01:22 +0900

    refactor: 테스트 케이스 이터레이션 구조로 변경

--- client/src/core/todo/test/validator.test.ts
+++ client/src/core/todo/test/validator.test.ts
@@ -1,4 +1,5 @@
-import * as rawTestCase from './data.json';
+import * as sortRawTestCase from './sort.data.json';
+import * as updateRawTestCase from './update.data.json';
 import { TestTodo, toTestTodo } from './type';
 import {
   validateImminenceSort,
@@ -9,13 +10,6 @@ import {
   validateWTL,
 } from './validator';
 
-import * as testCase1 from './temp2.json';
-import * as testCase2 from './temp7.json';
-import * as testCase3 from './temp9.json';
-import * as testCase4 from './temp-difficult0.json';
-import * as testCase5 from './temp-difficult1.json';
-import * as testCase6 from './temp-difficult4.json';
-
 const sort = (todoList: Array<TestTodo>): Array<TestTodo> => {
   return [...todoList];
 };
@@ -24,58 +18,49 @@ const update = (todoList: Array<TestTodo>): Array<TestTodo> => {
   return [...todoList];
 };
 
-const testCase = rawTestCase[0].data.map((el: any) => toTestTodo(el));
-const testToday = new Date(rawTestCase[0].today);
+const sortTestCase = sortRawTestCase.map((el) => ({
+  tag: el.tag,
+  today: new Date(el.today),
+  data: el.data.map((todo) => toTestTodo(todo)),
+}));
+const updateTestCase = updateRawTestCase.map((el) => ({
+  tag: el.tag,
+  today: new Date(el.today),
+  data: el.data.map((todo) => toTestTodo(todo)),
+}));
 
 describe('검증 알고리즘 테스트', () => {
-  let todoList: Array<TestTodo>;
-  beforeEach(() => {
-    todoList = testCase;
-  });
   describe('Imminence 정렬', () => {
-    it('Imminent Todo는 언제나 Distant Todo보다 우선순위가 높다', () => {
-      expect(validateImminenceSort(sort(todoList), testToday)).toBe(true);
+    test.each(sortTestCase)('$tag', ({ today, data }) => {
+      expect(validateImminenceSort(data, today)).toBe(true);
     });
   });
   describe('Importance 정렬', () => {
-    it('Imminence 정렬 조건 아래에서, Importance가 큰 Todo가 낮은 Todo보다 우선순위가 높다.', () => {
-      expect(validateImportanceSort(sort(todoList), testToday)).toBe(true);
+    test.each(sortTestCase)('$tag', ({ today, data }) => {
+      expect(validateImminenceSort(data, today)).toBe(true);
     });
   });
   describe('Deadline 정렬', () => {
-    it('Imminence와 Importance 정렬 조건 아래에서, Deadline이 가까운 Todo가 먼 Todo보다 우선순위가 높다.', () => {
-      expect(validateDeadlineSort(sort(todoList), testToday)).toBe(true);
+    test.each(sortTestCase)('$tag', ({ today, data }) => {
+      expect(validateImminenceSort(data, today)).toBe(true);
     });
   });
   describe('Last Postponed 정렬 ', () => {
-    it('Imminence, Importance, Deadline 정렬 조건 아래에서, Last Postponed가 가장 예전인 Todo가 현재에 가까운 Todo보다 우선순위가 높다.', () => {
-      expect(validateLastPostponedSort(sort(todoList), testToday)).toBe(true);
+    test.each(sortTestCase)('$tag', ({ today, data }) => {
+      expect(validateImminenceSort(data, today)).toBe(true);
     });
   });
 });
 
 describe('업데이트 검증 알고리즘 테스트', () => {
-  describe('RTL + WTL', () => {
-    it('RTL + WTL', () => {
-      const today = new Date('2023-1-1');
-      const todoList1 = testCase1.data.map((el) => toTestTodo(el));
-      const todoList2 = testCase2.data.map((el) => toTestTodo(el));
-      const todoList3 = testCase3.data.map((el) => toTestTodo(el));
-      const todoList4 = testCase4.data.map((el) => toTestTodo(el));
-      const todoList5 = testCase5.data.map((el) => toTestTodo(el));
-      const todoList6 = testCase6.data.map((el) => toTestTodo(el));
-      expect(validateRTL(update(todoList1), today)).toBe(true);
-      expect(validateRTL(update(todoList2), today)).toBe(true);
-      expect(validateRTL(update(todoList3), today)).toBe(true);
-      expect(validateRTL(update(todoList4), today)).toBe(true);
-      expect(validateRTL(update(todoList5), today)).toBe(true);
-      expect(validateRTL(update(todoList6), today)).toBe(true);
-      expect(validateWTL(update(todoList1), today)).toBe(true);
-      expect(validateWTL(update(todoList2), today)).toBe(true);
-      expect(validateWTL(update(todoList3), today)).toBe(true);
-      expect(validateWTL(update(todoList4), today)).toBe(true);
-      expect(validateWTL(update(todoList5), today)).toBe(true);
-      expect(validateWTL(update(todoList6), today)).toBe(true);
+  describe('RTL 조건 확인', () => {
+    test.each(updateTestCase)('$tag', ({ today, data }) => {
+      expect(validateRTL(data, today)).toBe(true);
+    });
+  });
+  describe('WTL 조건 확인', () => {
+    test.each(updateTestCase)('$tag', ({ today, data }) => {
+      expect(validateWTL(data, today)).toBe(true);
     });
   });
 });
