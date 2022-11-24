0d452addd96d2d8be44c3fc876362dca41b21e87
NaGyeong Park <ngp.july@gmail.com>
2022-11-24 14:20:29 +0900

    feat: 기본 Routing 설정

---
+++ client/src/Router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>hi</div>,
+  },
+]);
+
+export default {};
