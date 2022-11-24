7b627310409a58a5ce53e74564f9999eecc6c56f
kumsil1006 <39304306+kumsil1006@users.noreply.github.com>
2022-11-24 23:02:13 +0900

    feat: Sidebar 스타일 및 이벤트 구현, router 개선

--- client/src/container/Menubar.tsx
+++ client/src/container/Menubar.tsx
@@ -1,12 +1,36 @@
 import { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+
+import Button from '../components/Button';
+
+import Home from '../images/Home.svg';
+import Table from '../images/Table.svg';
 
 const Wrapper = styled.div`
   height: 100vh;
+  background: #fcfcfc;
+  box-shadow: 2px 0px 4px rgba(0, 0, 0, 0.25);
+  text-align: center;
 `;
 
 const Menubar = (): ReactElement => {
-  return <Wrapper>menu</Wrapper>;
+  const navigate = useNavigate();
+
+  const goHome = (): void => {
+    navigate('/');
+  };
+
+  const goTable = (): void => {
+    navigate('/table');
+  };
+
+  return (
+    <Wrapper>
+      <Button context={<img src={Home} />} margin={'30px 0 0 0'} onClick={goHome} />
+      <Button context={<img src={Table} />} margin={'30px 0 0 0'} onClick={goTable} />
+    </Wrapper>
+  );
 };
 
 export default Menubar;
