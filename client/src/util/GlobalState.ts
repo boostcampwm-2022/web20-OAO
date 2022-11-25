42f406f1bb997aa58495cf034675bc8b0f7636ea
NaGyeong Park <ngp.july@gmail.com>
2022-11-25 17:15:48 +0900

    feat: Main-TodoStatus container 구현

--- client/src/util/GlobalState.ts
+++ client/src/util/GlobalState.ts
@@ -1,8 +1,51 @@
 import { atom } from 'jotai';
 
-export const loginStateAtom = atom(false);
+export const loginStateAtom = atom(true);
+export const isOnProgress = atom('working');
 
 export const readWriteAtom = atom(
   (get) => get(loginStateAtom),
   (_get, set, newValue: boolean) => set(loginStateAtom, newValue),
 );
+
+export const mockTodosData = atom([
+  {
+    id: '0',
+    title: 'default title',
+    content: 'default content',
+    owner: 'default user',
+    importance: 1,
+    until: 'Mon Sep 03 2022 16:26:10 GMT+0900 (Korean Standard Time)',
+    from: 'Fri Sep 02 2022 12:39:39 GMT+0900 (Korean Standard Time)',
+    next: [],
+    prev: ['18'],
+    lastPostponed: '2022-11-22T09:18:36.208Z',
+    state: '',
+  },
+  {
+    id: '1',
+    title: 'default title',
+    content: 'default content',
+    owner: 'default user',
+    importance: 1,
+    until: 'Sat Nov 05 2022 10:25:59 GMT+0900 (Korean Standard Time)',
+    from: 'Sun Oct 30 2022 21:56:23 GMT+0900 (Korean Standard Time)',
+    next: [],
+    prev: [],
+    lastPostponed: '2022-11-22T09:18:36.208Z',
+    state: 'READY',
+  },
+  {
+    id: '2',
+    title: 'default title',
+    content: 'default content',
+    owner: 'default user',
+    importance: 1,
+    until: 'Sun Nov 27 2022 12:10:26 GMT+0900 (Korean Standard Time)',
+    from: 'Tue Nov 22 2022 15:37:02 GMT+0900 (Korean Standard Time)',
+    next: [],
+    prev: ['19'],
+    lastPostponed: '2022-11-22T09:18:36.208Z',
+    state: 'WAIT',
+  },
+]);
