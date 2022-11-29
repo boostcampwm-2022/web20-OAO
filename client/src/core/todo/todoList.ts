429ea9256f156b52594560b329fdc714eefd338c
n-ryu <ababc100564@gmail.com>
2022-11-29 16:53:47 +0900

    chore: todoList.util도 추가 분할

--- client/src/core/todo/todoList.ts
+++ client/src/core/todo/todoList.ts
@@ -1,33 +1,7 @@
 import { PlainTodo, InputTodo } from '@todo/todoList.type';
 import { Todo } from '@todo/todo';
 import { isEqualDate } from '@todo/todo.util';
-
-const compareFunctions = {
-  ascendImminence: (a: Todo, b: Todo): number => {
-    const newToday = new Date();
-    return -Number(isEqualDate(newToday, a.until)) + Number(isEqualDate(newToday, b.until));
-  },
-  descendImminence: (a: Todo, b: Todo): number => {
-    const newToday = new Date();
-    return Number(isEqualDate(newToday, a.until)) - Number(isEqualDate(newToday, b.until));
-  },
-  ascendDeadline: (a: Todo, b: Todo): number => a.until.getTime() - b.until.getTime(),
-  descendDeadline: (a: Todo, b: Todo): number => -a.until.getTime() + b.until.getTime(),
-  ascendImportance: (a: Todo, b: Todo): number => -b.importance + a.importance,
-  descendImportance: (a: Todo, b: Todo): number => b.importance - a.importance,
-  ascendLastPostponed: (a: Todo, b: Todo): number => a.lastPostponed.getTime() - b.lastPostponed.getTime(),
-  descendLastPostponed: (a: Todo, b: Todo): number => -a.lastPostponed.getTime() + b.lastPostponed.getTime(),
-  ascendTitle: (a: Todo, b: Todo): number => {
-    if (a.title === b.title) return 0;
-    if (a.title < b.title) return -1;
-    return 1;
-  },
-  descendTitle: (a: Todo, b: Todo): number => {
-    if (a.title === b.title) return 0;
-    if (a.title < b.title) return 1;
-    return -1;
-  },
-};
+import { compareFunctions } from '@todo/todoList.util';
 
 export class TodoList {
   private readonly todoList: Todo[];
