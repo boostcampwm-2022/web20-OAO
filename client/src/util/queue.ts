cc180ab67e4a4dd5016597e47e9fbb89da9680f2
n-ryu <ababc100564@gmail.com>
2022-12-07 01:10:56 +0900

    fix: queue의 length가 제대로 초기화되지 않던 에러 수정

--- client/src/util/queue.ts
+++ client/src/util/queue.ts
@@ -1,6 +1,6 @@
 interface QueueObj<T> {
-  prev: QueueObj<T> | undefined
-  next: QueueObj<T> | undefined
+  prev: QueueObj<T> | undefined;
+  next: QueueObj<T> | undefined;
   value: T | undefined;
 }
 
@@ -9,31 +9,39 @@ class Queue<T> {
   private tail: QueueObj<T>;
   #length: number;
   constructor(defaultArr: T[]) {
-    this.head = {prev: undefined, next: undefined, value: undefined};
-    this.tail = {prev: this.head, next: undefined, value: undefined};
+    this.head = { prev: undefined, next: undefined, value: undefined };
+    this.tail = { prev: this.head, next: undefined, value: undefined };
     this.head.next = this.tail;
-    defaultArr.forEach(el => this.push(el));
     this.#length = 0;
+    defaultArr.forEach((el) => this.push(el));
   }
-  length() : number {
+
+  length(): number {
     return this.#length;
   }
-  isEmpty() : boolean {
-    if(this.length() === 0) return true;
+
+  isEmpty(): boolean {
+    if (this.length() === 0) return true;
     return false;
   }
-  push(input: T): void {
-    const newQueueObj = {value: input, prev: this.tail.prev, next: this.tail};
-    if(this.tail.prev) this.tail.prev.next = newQueueObj;
+
+  private pushOne(input: T): void {
+    const newQueueObj = { value: input, prev: this.tail.prev, next: this.tail };
+    if (this.tail.prev != null) this.tail.prev.next = newQueueObj;
     this.tail.prev = newQueueObj;
-    this.#length ++;
+    this.#length++;
   }
-  pop(): T{
+
+  push(...input: T[]): void {
+    input.forEach((el) => this.pushOne(el));
+  }
+
+  pop(): T {
     const target = this.head.next;
-    if(this.isEmpty() || target === undefined) throw new Error("Error: You can't pop from empty queue");
+    if (this.isEmpty() || target === undefined) throw new Error('Error: You cant pop from empty queue');
     this.head.next = target.next;
-    if(target.next) target.next.prev = this.head;
-    this.#length --;
+    if (target.next != null) target.next.prev = this.head;
+    this.#length--;
     return target.value as T;
   }
 }
