638b503f3046fcc34a720ac4405831f08c5c1034
n-ryu <ababc100564@gmail.com>
2022-12-07 00:41:24 +0900

    feat: 최적화를 위해 Queue 직접 구현

---
+++ client/src/util/queue.ts
@@ -0,0 +1,40 @@
+interface QueueObj<T> {
+  prev: QueueObj<T> | undefined
+  next: QueueObj<T> | undefined
+  value: T | undefined;
+}
+
+class Queue<T> {
+  private head: QueueObj<T>;
+  private tail: QueueObj<T>;
+  #length: number;
+  constructor() {
+    this.head = {prev: undefined, next: undefined, value: undefined};
+    this.tail = {prev: this.head, next: undefined, value: undefined};
+    this.head.next = this.tail;
+    this.#length = 0;
+  }
+  length() : number {
+    return this.#length;
+  }
+  isEmpty() : boolean {
+    if(this.length() === 0) return true;
+    return false;
+  }
+  push(input: T): void {
+    const newQueueObj = {value: input, prev: this.tail.prev, next: this.tail};
+    if(this.tail.prev) this.tail.prev.next = newQueueObj;
+    this.tail.prev = newQueueObj;
+    this.#length ++;
+  }
+  pop(): T{
+    const target = this.head.next;
+    if(this.isEmpty() || target === undefined) throw new Error("Error: You can't pop from empty queue");
+    this.head.next = target.next;
+    if(target.next) target.next.prev = this.head;
+    this.#length --;
+    return target.value as T;
+  }
+}
+
+export default Queue;
