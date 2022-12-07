8e57e42fd140e99536483dce2da70f8676a2ac42
kumsil1006 <39304306+kumsil1006@users.noreply.github.com>
2022-12-07 20:12:18 +0900

    fix: postpone box default로 떠있는 상황 방지

--- client/src/hooks/useDone.ts
+++ client/src/hooks/useDone.ts
@@ -1,11 +1,12 @@
 import { useSetAtom, useAtom } from 'jotai';
 import { toast } from 'react-toastify';
 
-import { stopTimerAtom, todoList, elapsedTimeAtom } from '@util/GlobalState.js';
+import { stopTimerAtom, todoList, elapsedTimeAtom, postponeClicked } from '@util/GlobalState.js';
 
 const useDone = (): any[] => {
   const [todoListAtom, setTodoListAtom] = useAtom(todoList);
   const [elapsedTime, setElapsedTime] = useAtom(elapsedTimeAtom);
+  const setPostponeClicked = useSetAtom(postponeClicked);
   const stopTimer = useSetAtom(stopTimerAtom);
 
   const setDone = (): void => {
@@ -20,6 +21,7 @@ const useDone = (): any[] => {
         return await newTodoList.getActiveTodo();
       })
       .then((newActiveTodo) => {
+        if (newActiveTodo === undefined) setPostponeClicked(false);
         setElapsedTime(newActiveTodo !== undefined ? newActiveTodo.elapsedTime : 0);
         toast.success('완료되었습니다! 👏🏻👏🏻👏🏻');
       })
