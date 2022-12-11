964766ee5037706ebaebe9c6bf39c0ce3459c019
n-ryu <ababc100564@gmail.com>
2022-12-11 22:15:44 +0900

    213 micro feat 다이어그램 뷰 애니메이션 추가 (#214)
    * fix: GlobalStyle에 폰트 이름 잘못 들어가 있던 문제 해결
    
    * feat: 다이어그램 애니메이션 추가
    
    * fix: 폰트 제대로 설정되어 있지 않은 부분 수정
    
    * feat: 애니메이션을 위한 상태 관리 방식 변경 및 관련 함수 구현

--- client/src/components/diagram/TodoVertex.tsx
+++ client/src/components/diagram/TodoVertex.tsx
@@ -11,16 +11,39 @@ const Wrapper = styled.div`
   width: 0;
   height: 0;
   transform: translate(var(--x), var(--y));
+  transition: transform 1s;
   pointer-events: none;
   svg {
+    transition: transform 1s, width 1s, height 1s;
     pointer-events: none;
     path {
       pointer-events: stroke;
-      transition: stroke-width 0.3s;
+      transition: stroke-width 0.3s, d 1s;
       cursor: pointer;
     }
   }
 `;
+
+// const getPathValue = (
+//   x1: number,
+//   y1: number,
+//   x2: number,
+//   y2: number,
+// ): { path: string; width: number; height: number; viewBox: string; translateX: number; translateY: number } => {
+//   const p1 = { x: 0, y: 0 };
+//   const c1 = { x: 0, y: 0.4 * Math.abs(y2 - y1) };
+//   const c2 = { x: x2 - x1, y: y2 - y1 - 0.4 * Math.abs(y2 - y1) };
+//   const p2 = { x: x2 - x1, y: y2 - y1 };
+//   const start = { x: Math.min(p1.x, c1.x, c2.x, p2.x), y: Math.min(p1.y, c1.y, c2.y, p2.y) };
+//   const end = { x: Math.max(p1.x, c1.x, c2.x, p2.x), y: Math.max(p1.y, c1.y, c2.y, p2.y) };
+//   const path = `M${p1.x} ${p1.y}C${c1.x} ${c1.y} ${c2.x} ${c2.y} ${p2.x} ${p2.y}`;
+//   const width = end.x - start.x;
+//   const height = end.y - start.y;
+//   const viewBox = `${start.x} ${start.y} ${width} ${height}`;
+//   const translateX = -p1.x + start.x;
+//   const translateY = p1.y - start.y;
+//   return { path, width, height, viewBox, translateX, translateY };
+// };
 
 const getPathValue = (
   x1: number,
@@ -29,18 +52,25 @@ const getPathValue = (
   y2: number,
 ): { path: string; width: number; height: number; viewBox: string; translateX: number; translateY: number } => {
   const p1 = { x: 0, y: 0 };
-  const c1 = { x: 0, y: 0.4 * Math.abs(y2 - y1) };
-  const c2 = { x: x2 - x1, y: y2 - y1 - 0.4 * Math.abs(y2 - y1) };
-  const p2 = { x: x2 - x1, y: y2 - y1 };
+  const c1 = { x: 0, y: 0.4 };
+  const c2 = { x: Math.sign(x2 - x1), y: Math.sign(y2 - y1) - 0.4 };
+  const p2 = { x: Math.sign(x2 - x1), y: Math.sign(y2 - y1) };
   const start = { x: Math.min(p1.x, c1.x, c2.x, p2.x), y: Math.min(p1.y, c1.y, c2.y, p2.y) };
   const end = { x: Math.max(p1.x, c1.x, c2.x, p2.x), y: Math.max(p1.y, c1.y, c2.y, p2.y) };
   const path = `M${p1.x} ${p1.y}C${c1.x} ${c1.y} ${c2.x} ${c2.y} ${p2.x} ${p2.y}`;
   const width = end.x - start.x;
   const height = end.y - start.y;
   const viewBox = `${start.x} ${start.y} ${width} ${height}`;
-  const translateX = -p1.x + start.x;
-  const translateY = p1.y - start.y;
-  return { path, width, height, viewBox, translateX, translateY };
+  const translateX = (-p1.x + start.x) * Math.abs(x2 - x1);
+  const translateY = (p1.y - start.y) * Math.abs(y2 - y1);
+  return {
+    path,
+    width: width * Math.abs(x2 - x1),
+    height: height * Math.abs(y2 - y1),
+    viewBox,
+    translateX,
+    translateY,
+  };
 };
 
 const getColor = (type: 'NORMAL' | 'WARNING' | 'ERROR'): string => {
@@ -83,12 +113,14 @@ const Vertex = ({
       fill="none"
       transform={`translate(${translateX}, ${translateY})`}
       xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
     >
-      <path d={path} stroke={getColor(type)} strokeWidth={isHovered ? 4 : 2} />
+      <path d={path} stroke={getColor(type)} strokeWidth={isHovered ? 4 : 2} vectorEffect="non-scaling-stroke" />
       <path
         d={path}
         stroke="#00000000"
         strokeWidth={25}
+        vectorEffect="non-scaling-stroke"
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
         onMouseMove={getOnMouseMove(isHovered)}
